947b6cb0d88587e7e9226146e0cb70ba4f24188f
Muhammad Haseeb <mhaseeb37@gmail.com>
2023-10-20 22:09:43 +0500

    Removed Img tag from index
    Removed Img tag from index

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -29,7 +29,6 @@ export default function Homepage() {
       <Hero />
       <FeaturedCollection collection={data.featuredCollection} />
       <RecommendedProducts products={data.recommendedProducts} />
-      <img src='https://cdn.shopify.com/s/files/1/0829/4141/1633/products/NS_RTW23-35_F_cbfdac81-104c-4e0f-b4f4-f48bcb097dcd.jpg' />
       <CTA />
       <ProductList />
       <Logos />
