18859bb92b90e9432047233ae2fcf3c0cb73967c
Muhammad Haseeb <mhaseeb37@gmail.com>
2023-10-20 17:46:56 +0500

    Updated Vite Config
    Updated vide config

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,15 @@ import netlifyPlugin from '@netlify/hydrogen-platform/plugin'
 
 export default defineConfig({
   plugins: [hydrogen(), netlifyPlugin()],
-  //   ...
-})
+  resolve: {
+    alias: [{find: /^~\/(.*)/, replacement: '/app/$1'}],
+  },
+  optimizeDeps: {
+    include: ['@headlessui/react', 'clsx', 'react-use', 'typographic-base'],
+  },
+  test: {
+    globals: true,
+    testTimeout: 10000,
+    hookTimeout: 10000,
+  },
+});
