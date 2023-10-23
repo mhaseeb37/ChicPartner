cfebe810288cb2892d65f9cbce90d83328dd4d99
Muhammad Haseeb <mhaseeb37@gmail.com>
2023-10-23 18:30:43 +0500

    Removed ClassName from Header.jsx
    Removed Header classname

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -38,7 +38,7 @@ export function HeaderMenu({menu, viewport}) {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <nav className={className} role="navigation">
+    <nav role="navigation">
       <header className="absolute inset-x-0 top-0 z-50">
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
